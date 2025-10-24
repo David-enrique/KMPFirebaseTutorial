6d45a72b2beb2195bed3700a2407b11e0762788a
David-enrique <35897428+David-enrique@users.noreply.github.com>
2025-10-24 17:33:43 -0500

    Entorno composeApp

---
+++ KMPFirebaseTutorial/composeApp/src/commonTest/kotlin/com/davidhl/kmpfirebasetutorial/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.davidhl.kmpfirebasetutorial
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ KMPFirebaseTutorial/composeApp/src/iosMain/kotlin/com/davidhl/kmpfirebasetutorial/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.davidhl.kmpfirebasetutorial
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
